4db180422ea23ecd98e1d7e3a4eff17f2a2a9913
gabrielegiarratana000 <gabrielegiarratana000@gmail.com>
2021-10-13 17:06:18 +0200

    Args example

--- args.py
+++ args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     for opt, arg in opts:
         if opt == '-h':
             print
-            'test.py -i <inputfile> -o <outputfile>'
+            'date_test.py -i <inputfile> -o <outputfile>'
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
