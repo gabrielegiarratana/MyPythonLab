f3455d44f58c53fd7bdcce84b6532522dcfc10af
Gabriele Giarratana <gabrielegiarratana000@gmail.com>
2024-04-09 16:35:12 +0200

    Introducting pydantic

---
+++ src/pydantic/User.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, PositiveInt
+
+
+class User(BaseModel):
+    id: int
+    name: str = 'John Doe'
+    signup_ts: Optional[datetime]
+    tastes: dict[str, PositiveInt]
+
+
+external_data = {
+    'id': 123,
+    'signup_ts': '2019-06-01 12:22',
+    'tastes': {
+        'wine': 9,
+        b'cheese': 7,
+        'cabbage': '1',
+    },
+}
+
+
+if __name__ == '__main__':
+    user = User(**external_data)
+
+    print(user.id)
+    #> 123
+    print(user.model_dump())
+    """
+    {
+        'id': 123,
+        'name': 'John Doe',
+        'signup_ts': datetime.datetime(2019, 6, 1, 12, 22),
+        'tastes': {'wine': 9, 'cheese': 7, 'cabbage': 1},
+    }
+    """
